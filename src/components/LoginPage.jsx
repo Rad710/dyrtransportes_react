f740f114921fb9b2f3e0b96e08780b85c01a576f
Rad710 <rolmedro@gmail.com>
2023-10-05 14:06:24 -0300

    login netlify added

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,14 +1,29 @@
 import netlifyIdentity from 'netlify-identity-widget';
-import { useEffect } from 'react';
+import { Button } from '@radix-ui/themes';
+import { FileIcon, PersonIcon } from '@radix-ui/react-icons';
 
 const LoginPage = () => {
 
-  useEffect(() => {
-    netlifyIdentity.open("login")
-  }, [])
 
   return (
     <div className='bg-gray-400'>
+      <Theme
+        accentColor="indigo"
+        grayColor="gray"
+        radius="medium"
+      >
+        <Button color="indigo" variant="solid" size="4"
+          onClick={() => netlifyIdentity.open("login")}
+        >
+          <PersonIcon width="20" height="20" />Iniciar Sesión
+        </Button>
+
+        <Button color="indigo" variant="solid" size="4"
+          onClick={() => netlifyIdentity.open("signup")}
+        >
+          <FileIcon width="20" height="20" />Registrarse
+        </Button>
+      </Theme>
     </div>
   );
 };
