a77025073faac43a1eef027580fb66675625c8da
Rad710 <rolmedro@gmail.com>
2023-10-05 13:26:13 -0300

    login netlify added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,10 @@
-// LoginPage.js
-import React from 'react';
 import netlifyIdentity from 'netlify-identity-widget';
 
 const Login = () => {
-  const handleLogin = () => {
-    // Open the login/signup modal
-    netlifyIdentity.open();
-  };
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      <button onClick={handleLogin}>Log In</button>
+    <div className='bg-gray-500'>
+      {netlifyIdentity.open()}
     </div>
   );
 };
--- src/hooks/AuthWrapper.jsx
+++ src/hooks/AuthWrapper.jsx
@@ -1,29 +1,40 @@
 // AuthWrapper.js
-import React, { useEffect, useState } from 'react';
-import netlifyIdentity from 'netlify-identity-widget';
-import Login from '../components/Login';
+import { useEffect, useState } from 'react'
+import netlifyIdentity from 'netlify-identity-widget'
 
 const AuthWrapper = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     // Initialize the Netlify Identity widget
-    netlifyIdentity.init();
+    netlifyIdentity.init({
+      container: '#netlify-modal', // defaults to document.body
+      locale: 'es' // defaults to 'en'
+    })
 
     // Check the user's authentication status
-    const currentUser = netlifyIdentity.currentUser();
+    const currentUser = netlifyIdentity.currentUser()
 
     if (currentUser) {
-      setUser(currentUser);
+      setUser(currentUser)
     } else {
       // Listen for authentication events
       netlifyIdentity.on('login', (newUser) => {
         setUser(newUser);
-      });
-    }
-  }, []);
+      })
 
-  return <>{user ? children : <Login />}</>;
+      // Listen for sign-up events
+      netlifyIdentity.on('signup', (newUser) => {
+        setUser(newUser)
+      })
+    }
+  }, [])
+
+  return (
+    <>
+      {user ? children : <LoginPage />}
+    </>
+  )
 };
 
 export default AuthWrapper;
