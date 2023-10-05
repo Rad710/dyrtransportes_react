9cc63ea81bac5182f43494f4e39afb7e0d33ab6c
Rad710 <rolmedro@gmail.com>
2023-10-05 13:28:44 -0300

    login netlify added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,15 @@
 import netlifyIdentity from 'netlify-identity-widget';
+import { useEffect } from 'react';
 
 const Login = () => {
 
+  useEffect(() => {
+    netlifyIdentity.open()
+  }, [])
+
   return (
     <div className='bg-gray-500'>
-      {netlifyIdentity.open()}
+
     </div>
   );
 };
