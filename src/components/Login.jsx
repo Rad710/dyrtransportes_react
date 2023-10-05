c2d3a7b4bb8b14d291fb98af472e06bdc1da8f46
Rad710 <rolmedro@gmail.com>
2023-10-05 13:31:34 -0300

    login netlify added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,16 @@
-import netlifyIdentity from 'netlify-identity-widget';
-import { useEffect } from 'react';
+import { AvatarIcon } from '@radix-ui/react-icons';
+import { Button } from '@radix-ui/themes'
+import netlifyIdentity from 'netlify-identity-widget'
 
 const Login = () => {
 
-  useEffect(() => {
-    netlifyIdentity.open()
-  }, [])
-
   return (
     <div className='bg-gray-500'>
-
+      <Button color="indigo" variant="solid" size="4"
+        onClick={() => netlifyIdentity.open()}
+      >
+        <AvatarIcon width="20" height="20" />Iniciar Sesión
+      </Button>
     </div>
   );
 };
