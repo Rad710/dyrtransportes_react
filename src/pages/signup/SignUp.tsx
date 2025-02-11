40ff7a7c0432ad059d35c402c4d71eb7626262db
Rolando Medina Rosner <rolmedro@gmail.com>
2025-02-10 22:56:39 -0300

    fix: add overflow scroll to SignUp

--- src/pages/signup/SignUp.tsx
+++ src/pages/signup/SignUp.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
 import FormLabel from "@mui/material/FormLabel";
 import FormControl from "@mui/material/FormControl";
 import TextField from "@mui/material/TextField";
@@ -132,87 +131,80 @@ export const SignUp = () => {
     };
 
     return (
-        <>
-            <CssBaseline enableColorScheme />
+        <RegisterContainer direction="column" justifyContent="space-between" overflow="auto">
             <ColorModeSelect sx={{ position: "fixed", top: "1rem", right: "1rem" }} />
-            <RegisterContainer direction="column" justifyContent="space-between">
-                <Card variant="outlined">
-                    <DyRTransportesIcon />
-                    <Typography
-                        component="h1"
-                        variant="h4"
-                        sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
-                    >
-                        Sign up
-                    </Typography>
-                    {formErrorMessage && (
-                        <Box component="span" sx={{ color: (theme) => theme.palette.error.main }}>
-                            {formErrorMessage}
-                        </Box>
-                    )}
 
-                    <Box
-                        component="form"
-                        onSubmit={handleSubmit}
-                        sx={{ display: "flex", flexDirection: "column", gap: 2 }}
-                    >
-                        <FormControl>
-                            <FormLabel htmlFor="name">Full name</FormLabel>
-                            <TextField
-                                autoComplete="name"
-                                name="name"
-                                required
-                                fullWidth
-                                id="name"
-                                placeholder="Jon Snow"
-                                error={nameError}
-                                helperText={nameErrorMessage}
-                                color={nameError ? "error" : "primary"}
-                            />
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel htmlFor="email">Email</FormLabel>
-                            <TextField
-                                required
-                                fullWidth
-                                id="email"
-                                placeholder="your@email.com"
-                                name="email"
-                                autoComplete="email"
-                                variant="outlined"
-                                error={emailError}
-                                helperText={emailErrorMessage}
-                                color={passwordError ? "error" : "primary"}
-                            />
-                        </FormControl>
-                        <FormControl>
-                            <FormLabel htmlFor="password">Password</FormLabel>
-                            <TextField
-                                required
-                                fullWidth
-                                name="password"
-                                placeholder="••••••"
-                                type="password"
-                                id="password"
-                                autoComplete="new-password"
-                                variant="outlined"
-                                error={passwordError}
-                                helperText={passwordErrorMessage}
-                                color={passwordError ? "error" : "primary"}
-                            />
-                        </FormControl>
-
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            onClick={validateInputs}
-                        >
-                            Sign up
-                        </Button>
+            <Card variant="outlined" sx={{ overflowY: "visible" }}>
+                <DyRTransportesIcon />
+                <Typography
+                    component="h1"
+                    variant="h4"
+                    sx={{ width: "100%", fontSize: "clamp(2rem, 10vw, 2.15rem)" }}
+                >
+                    Sign up
+                </Typography>
+                {formErrorMessage && (
+                    <Box component="span" sx={{ color: (theme) => theme.palette.error.main }}>
+                        {formErrorMessage}
                     </Box>
-                </Card>
-            </RegisterContainer>
-        </>
+                )}
+
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                    sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+                >
+                    <FormControl>
+                        <FormLabel htmlFor="name">Full name</FormLabel>
+                        <TextField
+                            autoComplete="name"
+                            name="name"
+                            required
+                            fullWidth
+                            id="name"
+                            placeholder="Jon Snow"
+                            error={nameError}
+                            helperText={nameErrorMessage}
+                            color={nameError ? "error" : "primary"}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel htmlFor="email">Email</FormLabel>
+                        <TextField
+                            required
+                            fullWidth
+                            id="email"
+                            placeholder="your@email.com"
+                            name="email"
+                            autoComplete="email"
+                            variant="outlined"
+                            error={emailError}
+                            helperText={emailErrorMessage}
+                            color={passwordError ? "error" : "primary"}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <FormLabel htmlFor="password">Password</FormLabel>
+                        <TextField
+                            required
+                            fullWidth
+                            name="password"
+                            placeholder="••••••"
+                            type="password"
+                            id="password"
+                            autoComplete="new-password"
+                            variant="outlined"
+                            error={passwordError}
+                            helperText={passwordErrorMessage}
+                            color={passwordError ? "error" : "primary"}
+                        />
+                    </FormControl>
+
+                    <Button type="submit" fullWidth variant="contained" onClick={validateInputs}>
+                        Sign up
+                    </Button>
+                </Box>
+            </Card>
+        </RegisterContainer>
     );
 };
