aa7cb462be90dad38eaf42c0f68c7a448c67e855
Rolando Medina Rosner <rolmedro@gmail.com>
2025-03-26 22:51:58 -0300

    fix: app bar icon and main icon .svg path

--- src/layout/components/ResponsiveAppBar.tsx
+++ src/layout/components/ResponsiveAppBar.tsx
@@ -223,7 +223,7 @@ export const ResponsiveAppBar = () => {
                     <Box sx={{ flexGrow: 0 }}>
                         <Tooltip title={t("userMenu.openSettings")}>
                             <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                <Avatar />
                             </IconButton>
                         </Tooltip>
                         <Menu
