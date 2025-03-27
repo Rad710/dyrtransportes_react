f633279e79158e92aeac341346e442d99409ed94
Rolando Medina Rosner <rolmedro@gmail.com>
2025-03-27 20:30:10 -0300

    feat: added translations to app bar

--- src/layout/components/ResponsiveAppBar.tsx
+++ src/layout/components/ResponsiveAppBar.tsx
@@ -17,7 +17,50 @@ import MenuItem from "@mui/material/MenuItem";
 
 import { useAuthStore } from "@/stores/authStore";
 import { DyRTransportesIcon } from "@/components/DyRTransportesIcon";
-import { appBarTranslationNamespace } from "../translations";
+
+import i18n, { appLanguages } from "@/utils/i18n";
+
+const resources = {
+    en: {
+        translation: {
+            navigation: {
+                shipmentPayrolls: "Shipment Payrolls",
+                driverPayrolls: "Driver Payrolls",
+                routes: "Routes",
+                drivers: "Drivers",
+                dinatran: "DINATRAN",
+            },
+            userMenu: {
+                openSettings: "Open settings",
+                profile: "Profile",
+                logout: "Logout",
+            },
+        },
+    },
+    es: {
+        translation: {
+            navigation: {
+                shipmentPayrolls: "Cobranzas",
+                driverPayrolls: "Liquidaciones",
+                routes: "Precios y Productos",
+                drivers: "Choferes",
+                dinatran: "DINATRAN",
+            },
+            userMenu: {
+                openSettings: "Abrir configuración",
+                profile: "Perfil",
+                logout: "Cerrar sesión",
+            },
+        },
+    },
+};
+
+const appBarTranslationNamespace = "appBar";
+appLanguages.forEach((lang) => {
+    if (!i18n.hasResourceBundle(lang, appBarTranslationNamespace)) {
+        i18n.addResourceBundle(lang, appBarTranslationNamespace, resources[lang].translation);
+    }
+});
 
 interface AppMenuLink {
     name: string;
